ec504a1863ae130eff3822e72d3c129b832c7aeb
Elliott Pardee <me@thevypr.com>
2015-06-07 03:59:45 -0400

    Update typeprint.go

--- typeprint.go
+++ typeprint.go
@@ -1,18 +1,1 @@
-package main
 
-import (
-    "fmt"
-    "time"
-)
-
-func typeprint(s string) {
-    for _, j := range s {
-        if string(j) == "\n" {
-            fmt.Printf("\n")
-        } else {
-            fmt.Printf("%c", j)
-        }
-
-        time.Sleep(time.Millisecond * time.Duration(10))
-    }
-}
