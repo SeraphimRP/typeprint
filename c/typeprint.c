6ce020240b1dd356a32d3b1a2288f6efb98bf1f4
Elliott Pardee <me@elliottpardee.me>
2015-06-12 04:23:06 -0400

    some error checking

--- c/typeprint.c
+++ c/typeprint.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 static int process(char *name);
 
@@ -14,9 +15,14 @@ process(char *name)
         ts.tv_sec = 0;
         ts.tv_nsec = 10 * 1000000;
 
-        if (file == 0) {
+        if (access(file, F_OK) = -1) {
             fprintf(stderr, "[err] no such file or directory\n");
             return ENOENT;
+        }
+
+        if (access(file, R_OK) = -1) {
+            fprintf(stderr, "[err] can't read file, permission denied\n");
+            return EACCES;
         }
 
         int x;
